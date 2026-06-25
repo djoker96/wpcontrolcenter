3c78951d06244de75ab9a721d2f4fbb94e2b4a01
djoker96 <datjoker96@gmail.com>
2026-06-25 10:20:59 +0700

    feat(auth): add email verification registration

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { EmailVerificationService } from './email-verification.service';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
+  imports: [MailModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, EmailVerificationService],
   exports: [AuthService],
 })
 export class AuthModule {}
